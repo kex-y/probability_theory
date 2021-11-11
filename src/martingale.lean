8ba84d7c15e905b7ab8c87da7176011374a73596
Kexing <kexing.ying19@imperial.ac.uk>
2021-11-11 18:22:14 +0000

    clean

--- src/martingale.lean
+++ src/martingale.lean
@@ -1,7 +1,7 @@
 import measure_theory.function.conditional_expectation
 
 noncomputable theory
-open_locale classical measure_theory nnreal ennreal
+open_locale classical measure_theory nnreal ennreal topological_space
 
 namespace measure_theory
 
@@ -13,24 +13,28 @@ structure filtration (α ι : Type*) [preorder ι] (m : measurable_space α) :=
 
 open topological_space
 
-variables {α β ι : Type*} [preorder ι] {m : measurable_space α} [measurable_space β]
+variables {α β ι : Type*} {m : measurable_space α} [measurable_space β]
 
 /-- A measure is σ-finite with respect to filtration if it is σ-finite with respect 
 to all the sub-σ-algebra of the filtration. -/
-class sigma_finite_filtration (μ : measure α) (f : filtration α ι m) : Prop :=
+class sigma_finite_filtration [preorder ι] (μ : measure α) (f : filtration α ι m) : Prop :=
 (sigma_finite : ∀ ⦃i : ι⦄, @sigma_finite α (f.seq i) (μ.trim (f.le i)))
 
-instance sigma_finite_of_sigma_finite_filtration (μ : measure α) (f : filtration α ι m)
+instance sigma_finite_of_sigma_finite_filtration [preorder ι] (μ : measure α) (f : filtration α ι m)
   [hf : sigma_finite_filtration μ f] (i : ι) : 
   @sigma_finite α (f.seq i) (μ.trim (f.le i)) := 
 by apply hf.sigma_finite -- can't exact here
 
 /-- A sequence of functions `u` is adapted to a filtration `f` if for all `i`, 
 `u i` is `f i`-measurable. -/
-def adapted (f : filtration α ι m) (u : ι → α → β) : Prop := 
+def adapted [preorder ι] (f : filtration α ι m) (u : ι → α → β) : Prop := 
 ∀ ⦃i : ι⦄, measurable[f.seq i] (u i)
 
 namespace filtration
+
+section
+
+variable [preorder ι]
 
 /-- Given a sequence of functions, the natural filtration is the smallest sequence 
 of σ-algebras such that that sequence of functions is measurable with respect to 
@@ -54,13 +58,17 @@ section is_martingale
 variables {E : Type*} [normed_group E] [measurable_space E]  
   [second_countable_topology E] [normed_space ℝ E] [complete_space E] [borel_space E]
 
-def is_martingale (μ : measure α) (f : filtration α ι m) (u : ι → α → E) : Prop :=
+-- def is_submartingale (μ : measure α) (f : filtration α ι m) (u : ι → Lp E 1 μ) : Prop :=
+-- ∀ ⦃i j : ι⦄, i ≤ j → ∀ ⦃s : set α⦄, measurable_set[f.seq i] s →
+--   ∫ x in s, u i x ∂μ ≤ ∫ x in s, u j x ∂μ
+  
+def is_martingale (μ : measure α) (f : filtration α ι m) (u : ι → Lp E 1 μ) : Prop :=
 ∀ ⦃i j : ι⦄, i ≤ j → ∀ ⦃s : set α⦄, measurable_set[f.seq i] s →
   ∫ x in s, u i x ∂μ = ∫ x in s, u j x ∂μ
 
 -- Could make `u` measurable with respect to `f` but that would be less general
 lemma condexp_eq_of_is_martingale (μ : measure α) (f : filtration α ι m) 
-  [sigma_finite_filtration μ f] {u : ι → α → E} (hum : is_martingale μ f u) 
+  [sigma_finite_filtration μ f] {u : ι →  Lp E 1 μ} (hum : is_martingale μ f u) 
   (hui : ∀ i, integrable (u i) μ) {i j : ι} (hij : i ≤ j) 
   (huim : measurable[f.seq i] (u i)) : u i =ᵐ[μ] μ[u j | f.le i] := 
 begin
@@ -71,6 +79,8 @@ begin
 end
 
 end is_martingale
+
+end
 
 end filtration
 
